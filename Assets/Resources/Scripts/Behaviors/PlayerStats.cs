061014d97ec575c83ff8871d0866f79fd357946c
vincent12322 <eber0795@fredonia.edu>
2018-04-12 14:33:11 -0400

    Need feedback on winning conditions

---
+++ Assets/Resources/Scripts/Behaviors/PlayerStats.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinningConditions : MonoBehaviour
+{
+  private int eliminations{ get; private set; }
+  private int outs { get; private set; }
+  private int playerNumber { get: private set;  } // This is to identify each player seperately
+}
