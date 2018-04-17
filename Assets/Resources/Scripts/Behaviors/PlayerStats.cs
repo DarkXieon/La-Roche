5c76975badc2414ec1c3e854d5325e7caa9115ac
DarkXieon <andrewkausy@gmail.com>
2018-04-17 01:03:17 -0400

    Bug Fixes

--- Assets/Resources/Scripts/Behaviors/PlayerStats.cs
+++ Assets/Resources/Scripts/Behaviors/PlayerStats.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
-  private int eliminations{ get; private set; }
-  private int outs { get; private set; }
-  private int playerNumber { get: private set;  } // This is to identify each player seperately
+    public int Eliminations{ get; private set; }
+    public int Outs { get; private set; }
+    public int PlayerNumber { get; private set; } // This is to identify each player seperately
 }
